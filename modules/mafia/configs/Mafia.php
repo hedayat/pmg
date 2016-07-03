9cc2aac16c7bf5550c1f5dc96fa560b21887acff
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2016-07-03 05:59:28 +0430

    Complete mafia help

--- modules/mafia/configs/Mafia.php
+++ modules/mafia/configs/Mafia.php
@@ -10,14 +10,14 @@ use awesomeircbot\module\ModuleConfig;
 use awesomeircbot\line\ReceivedLineTypes;
 
 class Mafia implements ModuleConfig {
-	
+
 	public static $mappedCommands = array(
 		"list"	=>  "modules\mafia\MafiaList",
 		"vote"	=>  "modules\mafia\MafiaVote",
 		"heal"	=>  "modules\mafia\MafiaHeal",
 		"whois"	=>  "modules\mafia\MafiaWhois",
 		"opt"	=>  "modules\mafia\MafiaOpt",
-		"upd"     => "modules\mafia\MafiaRaw",
+		"raw"     => "modules\mafia\MafiaRaw",
 		"kick"     => "modules\mafia\MafiaKick",
 		"drop"     => "modules\mafia\MafiaDrop",
 		"register" => "modules\mafia\MafiaJoinGame",
@@ -38,72 +38,197 @@ class Mafia implements ModuleConfig {
 		"slap" => "modules\mafia\MafiaSlap",
 		"mafia" => "modules\mafia\MafiaMafia",
 	);
-	
+
 	public static $mappedEvents = array(
 		ReceivedLineTypes::JOIN => "modules\mafia\MafiaJoinChanel",
 		ReceivedLineTypes::NICK => "modules\mafia\MafiaNick",
 	);
-	
+
 	public static $mappedTriggers = array(
 	);
 
 	public static $help = array(
+		"MAFIA COMMANDS: (" => array(
+			"BASE" => array(
+				"description" => "Not a command itself!",
+				"parameters" => false
+			)
+		),
 		"register" => array(
 			"BASE" => array(
 				"description" => "Join to the game",
-				"parameters" => ""
+				"parameters" => false
 			)
 		),
 		"leave" => array(
 			"BASE" => array(
 				"description" => "Leave the game",
-				"parameters" => ""
+				"parameters" => false
 			)
 		),
-		"kill" => array(
+		"opt" => array(
 			"BASE" => array(
-				"description" => "Mafia command, kill a ppl",
-				"parameters" => "nick"
+				"description" => "Show current options",
+				"parameters" => false
+			),
+			"show-mafia" => array(
+				"description" => "Show identity on day punish",
+				"parameters" => "0|1"
+			),
+			"mafia-state" => array(
+				"description" => "Set when mafia will win: 0: mafia count = ppl count, 1: mafia count = 0",
+				"parameters" => "0|1"
+			),
+			"dead-talk" => array(
+				"description" => "Set if dead can talk (1: dead can talk, 0: dead cannot talk)",
+				"parameters" => "0|1"
+			),
+			"night-time" => array(
+				"description" => "Minimum allowed night time (at least 100)",
+				"parameters" => "<seconds>"
+			),
+			"day-time" => array(
+				"description" => "Minimum allowed day time (at least 100)",
+				"parameters" => "<seconds>"
 			)
 		),
-		"punish" => array(
+		"name" => array(
 			"BASE" => array(
-				"description" => "Day command punish a ppl",
-				"parameters" => "nick"
+				"description" => "Set the name of the game",
+				"parameters" => "<game_name>"
+			)
+		),
+		"save" => array(
+			"BASE" => array(
+				"description" => "Save current game and optionally set the name of the current game",
+				"parameters" => "[<game_name>]"
+			)
+		),
+		"load" => array(
+			"BASE" => array(
+				"description" => "Load saved game and optionally set the name of the current game",
+				"parameters" => "[<game_name>]"
 			)
 		),
 		"start" => array(
 			"BASE" => array(
 				"description" => "Start game",
-				"parameters" => "mafiacount havedr"
+				"parameters" => "<mafiacount> [<have_dr> [<have_detective> [<have_invulnerable>]]]"
 			)
-		),	
+		),
 		"restart" => array(
 			"BASE" => array(
 				"description" => "ReStart game to register again",
-				"parameters" => ""
+				"parameters" => false
 			)
 		),
-		
-		"raw" => array(
+		"whoami" => array(
 			"BASE" => array(
-				"description" => "Send raw message ",
-				"parameters" => "rawmessage"
+				"description" => "Ask who you are in the current game",
+				"parameters" => false
 			)
 		),
-		"heal" => array(
+		"count" => array(
 			"BASE" => array(
-				"description" => "Heal in night time",
-				"parameters" => "pplname"
+				"description" => "Prints the number of players",
+				"parameters" => false
 			)
 		),
 		"list" => array(
 			"BASE" => array(
 				"description" => "List all registered and in game state",
-				"parameters" => ""
+				"parameters" => false
 			)
-		),				
+		),
+		"vote" => array(
+			"BASE" => array(
+				"description" => "List current votes",
+				"parameters" => false
+			)
+		),
+		"punish" => array(
+			"BASE" => array(
+				"description" => "Day command punish a ppl",
+				"parameters" => "<nick>"
+			)
+		),
+		"timeout" => array(
+			"BASE" => array(
+				"description" => "Request timeout",
+				"parameters" => false
+			)
+		),
+		"kill" => array(
+			"BASE" => array(
+				"description" => "Mafia command, kill a ppl",
+				"parameters" => "<nick>"
+			)
+		),
+		"heal" => array(
+			"BASE" => array(
+				"description" => "Heal in night time",
+				"parameters" => "<nick>"
+			)
+		),
+		"whois" => array(
+			"BASE" => array(
+				"description" => "Detector command in night time to ask if someone is mafia",
+				"parameters" => "<nick>"
+			)
+		),
+		"wish" => array(
+			"BASE" => array(
+				"description" => "A wish by the last one dead at night",
+				"parameters" => "<wish>"
+			)
+		),
+		"mafia" => array(
+			"BASE" => array(
+				"description" => "Say you think someone is mafia",
+				"parameters" => "<nick> [<reason>]"
+			)
+		),
+		"slap" => array(
+			"BASE" => array(
+				"description" => "Slap somebody!",
+				"parameters" => "<nick> [<reason>]"
+		"voice" => array(
+			"BASE" => array(
+				"description" => "Ask for voice, if you should have it but don't",
+				"parameters" => false
+			)
+		),
+		"validate" => array(
+			"BASE" => array(
+				"description" => "Remove a user which has left channel from the game",
+				"parameters" => "<nick>"
+			)
+		),
+		" ) " => array(
+			"BASE" => array(
+				"description" => "Not a command itself!",
+				"parameters" => false
+			)
+		),
+		"kick" => array(
+			"BASE" => array(
+				"description" => "Kick a user out of channel",
+				"parameters" => false
+			)
+		),
+		"drop" => array(
+			"BASE" => array(
+				"description" => "Remove a nick from game",
+				"parameters" => false
+			)
+		),
+		"raw" => array(
+			"BASE" => array(
+				"description" => "Send raw message ",
+				"parameters" => "<rawmessage>"
+			)
+		),
 	);
-			
+
 }
 ?>
