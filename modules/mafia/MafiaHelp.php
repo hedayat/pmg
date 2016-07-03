2464e49dfa8c9d4bc71c3d498f8bfd5e433bcd4b
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2016-07-03 18:35:37 +0430

    Use notify for help, to see if it prevents flood!

--- modules/mafia/MafiaHelp.php
+++ modules/mafia/MafiaHelp.php
@@ -13,40 +13,40 @@ class MafiaHelp extends Module {
     public function run() {
         $server = Server::getInstance();
         $I = $this->senderNick;
-        $server->message($I, MafiaGame::bold("Day-only Commands:"));
-        $server->message($I, "!punish <nick>  : To vote to punish <nick>");
-        $server->message($I, "!punish -  : To remove your vote");
-        $server->message($I, "!vote  : To see other people votes");
-        $server->message($I, "!timeout  : To end the day after " . MafiaGame::$DAY_TIMEOUT . " and 60% of player cast their vote.");
+        $server->notify($I, MafiaGame::bold("Day-only Commands:"));
+        $server->notify($I, "!punish <nick>  : To vote to punish <nick>");
+        $server->notify($I, "!punish -  : To remove your vote");
+        $server->notify($I, "!vote  : To see other people votes");
+        $server->notify($I, "!timeout  : To end the day after " . MafiaGame::$DAY_TIMEOUT . " and 60% of player cast their vote.");
 
-        $server->message($I, MafiaGame::bold("Night-only Commands:"));
-        $server->message($I, "!timeout  : To end the night after " . MafiaGame::$NIGHT_TIMEOUT);
+        $server->notify($I, MafiaGame::bold("Night-only Commands:"));
+        $server->notify($I, "!timeout  : To end the night after " . MafiaGame::$NIGHT_TIMEOUT);
 
-        $server->message($I, MafiaGame::bold("Doctor Command:"));
-        $server->message($I, "!heal <nick>  : To heal <nick>");
+        $server->notify($I, MafiaGame::bold("Doctor Command:"));
+        $server->notify($I, "!heal <nick>  : To heal <nick>");
 
-        $server->message($I, MafiaGame::bold("Detector Command:"));
-        $server->message($I, "!whois <nick>  : To investigate <nick>");
+        $server->notify($I, MafiaGame::bold("Detector Command:"));
+        $server->notify($I, "!whois <nick>  : To investigate <nick>");
 
-        $server->message($I, MafiaGame::bold("Mafia Commands:"));
-        $server->message($I, "!kill <nick>  : To kill <nick>");
-        $server->message($I, "!kill *  : To kill nobody");
-        $server->message($I, "!kill -  : To remove your vote");
-        $server->message($I, "!vote  : To see other mafias (and their votes)");
+        $server->notify($I, MafiaGame::bold("Mafia Commands:"));
+        $server->notify($I, "!kill <nick>  : To kill <nick>");
+        $server->notify($I, "!kill *  : To kill nobody");
+        $server->notify($I, "!kill -  : To remove your vote");
+        $server->notify($I, "!vote  : To see other mafias (and their votes)");
 
-        $server->message($I, MafiaGame::bold("Global Commands:"));
-        $server->message($I, "!list  : To see list of all players");
-        $server->message($I, "!count : To see the number of players");
-        $server->message($I, "!whoami: To see who you are!");
-        $server->message($I, "!voice : If you must have voice and you have no voice, (mostly reconnect problems)");
+        $server->notify($I, MafiaGame::bold("Global Commands:"));
+        $server->notify($I, "!list  : To see list of all players");
+        $server->notify($I, "!count : To see the number of players");
+        $server->notify($I, "!whoami: To see who you are!");
+        $server->notify($I, "!voice : If you must have voice and you have no voice, (mostly reconnect problems)");
 
-        $server->message($I, MafiaGame::bold("Dead Commands:"));
-        $server->message($I, "!wish <wish> : To say your last wish after being killed at night!");
-        $server->message($I, "!slap <nick> [<reason>] : To slap somebody!");
-        $server->message($I, "!mafia <nick> [<reason>] : To say you think someone is mafia!");
+        $server->notify($I, MafiaGame::bold("Dead Commands:"));
+        $server->notify($I, "!wish <wish> : To say your last wish after being killed at night!");
+        $server->notify($I, "!slap <nick> [<reason>] : To slap somebody!");
+        $server->notify($I, "!mafia <nick> [<reason>] : To say you think someone is mafia!");
 
-        $server->message($I, MafiaGame::bold("Other Commands:"));
-        $server->message($I, "register, leave, start, restart, validate, opt, name, save, load, kick, drop, raw");
+        $server->notify($I, MafiaGame::bold("Other Commands:"));
+        $server->notify($I, "register, leave, start, restart, validate, opt, name, save, load, kick, drop, raw");
     }
 
 }
